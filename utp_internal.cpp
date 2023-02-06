22a6b778d2d9739a0209a05cf39d9205a296b415
tinselcity <reed.morrison@gmail.com>
2023-02-06 15:57:43 -0800

    wrapping debugging only globals in ifdefs

--- utp_internal.cpp
+++ utp_internal.cpp
@@ -154,9 +154,11 @@ enum {
 	ST_NUM_STATES,		// used for bounds checking
 };
 
+#if UTP_DEBUG_LOGGING
 static const cstr flagnames[] = {
 	"ST_DATA","ST_FIN","ST_STATE","ST_RESET","ST_SYN"
 };
+#endif
 
 enum CONN_STATE {
 	CS_UNINITIALIZED = 0,
@@ -169,9 +171,11 @@ enum CONN_STATE {
 	CS_DESTROY
 };
 
+#if UTP_DEBUG_LOGGING
 static const cstr statenames[] = {
 	"UNINITIALIZED", "IDLE","SYN_SENT", "SYN_RECV", "CONNECTED","CONNECTED_FULL","DESTROY_DELAY","RESET","DESTROY"
 };
+#endif
 
 struct OutgoingPacket {
 	size_t length;
